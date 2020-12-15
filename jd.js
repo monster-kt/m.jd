42c1466104f9549aca191db7e1ae150a366f05e8
surui <1365001944@qq.com>
2020-12-15 16:42:19 +0800

    对基本页面进行优化 添加了flag 进行判断优化；还阻止了屏幕滚动

--- jd.js
+++ jd.js
@@ -7,21 +7,25 @@
  */
 window.addEventListener('load',function () {
     // 获取最大的盒子
-    var banner = document.querySelector('.banner');
+    let banner = document.querySelector('.banner');
     // 获取ul
-    var ul = banner.children[0];
+    let ul = banner.children[0];
     // 获取ol
-    var ol = banner.children[1];
+    let ol = banner.children[1];
     let count = 0;
     // 获取banner宽度
     let w = banner.offsetWidth;
     // 过渡时间
-    var time = '';
+    let time = '';
+    // 判断用户是否拖动
+    let flag = false;
+
     // 利用定时器自动轮播
-    var timer = setInterval(function () {
+    let timer = setInterval(function () {
         count++;
         movepic(count,'.3s')
     },2000);
+
     // 等待过渡完成了 再去判断  监听过渡完成的事件 transitionend
     ul.addEventListener('transitionend',function () {
         // 无缝滚动
@@ -37,36 +41,44 @@ window.addEventListener('load',function () {
         ol.querySelector('.current').classList.remove('current');
         ol.children[count].classList.add('current');
     });
+
     // 手指滑动轮播图
-    let x = 0;
-    let movex = 0;
+    let x = 0;               // 开始位置
+    let movex = 0;          // 移动距离
     ul.addEventListener('touchstart',function (e) {
         x = e.targetTouches[0].pageX;
         clearInterval(timer);
     });
+
     ul.addEventListener('touchmove',function (e) {
+        flag = true;        // 只有用户手指移动了我们再判断是否需要移动图片 否则不做判断
         movex = e.targetTouches[0].pageX - x;
         // 手指拖动不需要动画
         let translateX = -count * w + movex;
         this.style.transition = 'none';
         this.style.transform = 'translateX('+ translateX +'px)';
-    });
-    // 手指滑动播放上下张
-    ul.addEventListener('touchend',function () {
-        if (Math.abs(movex) > 50){
-            if (movex > 0){
-                count--;
-            }else {
-                count++;
-            }
-        }
-        movepic(count,'.3s');
-        timer = setInterval(function () {
-            count++;
-            movepic(count,'.3s')
-        },2000);
+        e.preventDefault();             // 阻止屏幕滚动
     });
 
+    // 手指滑动播放上下张
+    ul.addEventListener('touchend',function () {
+        if (flag) {
+            if (Math.abs(movex) > 50) {
+                if (movex > 0) {
+                    count--;
+                } else {
+                    count++;
+                }
+            }
+            movepic(count, '.3s');
+            timer = setInterval(function () {
+                count++;
+                movepic(count, '.3s')
+            }, 2000);
+        }
+    });
+
+    // 封装函数
     function movepic(count,time) {
         let translatex = -count * w;
         ul.style.transition = time;
